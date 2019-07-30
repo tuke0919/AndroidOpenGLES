bbb4858b91c22643386298188c77a29257b7f177
tuke <tuke@corp.netease.com>
2019-07-30 10:47:19 +0800

    修改c++实现

--- openglcamera/src/main/jni/ImageBeauty.cpp
+++ openglcamera/src/main/jni/ImageBeauty.cpp
@@ -1,4 +1,58 @@
-//
-// Created by KE TU on 2019/7/29.
-//
 
+#include "com_yinge_opengl_camera_image_ImageBeautyUtil.h"
+
+/*
+ * Class:     com_yinge_opengl_camera_image_ImageBeautyUtil
+ * Method:    native_storeBitmap
+ * Signature: (Landroid/graphics/Bitmap;)V
+ */
+JNIEXPORT void JNICALL Java_com_yinge_opengl_camera_image_ImageBeautyUtil_native_1storeBitmap
+        (JNIEnv *, jobject, jobject){
+
+
+}
+
+/*
+ * Class:     com_yinge_opengl_camera_image_ImageBeautyUtil
+ * Method:    native_freeBitmap
+ * Signature: (Ljava/nio/ByteBuffer;)V
+ */
+JNIEXPORT void JNICALL Java_com_yinge_opengl_camera_image_ImageBeautyUtil_native_1freeBitmap
+        (JNIEnv *, jobject, jobject){
+
+
+}
+
+/*
+ * Class:     com_yinge_opengl_camera_image_ImageBeautyUtil
+ * Method:    native_getBitmap
+ * Signature: (Ljava/nio/ByteBuffer;)Landroid/graphics/Bitmap;
+ */
+JNIEXPORT jobject JNICALL Java_com_yinge_opengl_camera_image_ImageBeautyUtil_native_1getBitmap
+        (JNIEnv *, jobject, jobject){
+
+
+}
+
+/*
+ * Class:     com_yinge_opengl_camera_image_ImageBeautyUtil
+ * Method:    native_initBeauty
+ * Signature: (Ljava/nio/ByteBuffer;)V
+ */
+JNIEXPORT void JNICALL Java_com_yinge_opengl_camera_image_ImageBeautyUtil_native_1initBeauty
+        (JNIEnv *, jobject, jobject){
+
+
+}
+
+/*
+ * Class:     com_yinge_opengl_camera_image_ImageBeautyUtil
+ * Method:    native_freeBeauty
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_yinge_opengl_camera_image_ImageBeautyUtil_native_1freeBeauty
+        (JNIEnv *, jobject){
+
+
+
+}
